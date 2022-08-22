ddb94f8a29ef0bb89a4ce9e20aca036ee52dcce3
Rayane-T <slamaper1@gmail.com>
2022-08-23 00:42:34 +0200

    Update 0.2 (Big Update)
    - Email.js

--- project.js
+++ project.js
@@ -2,10 +2,4 @@ function politique() {
     alert('La politique de confidentialité signifie que votre message peut être utilisé et transmis à des fins commerciales de manière anonyme.');
 };
 
-function check() {
-    if (document.getElementById("checkbox1").checked = true {
-        alert("test")
-    }
-}
 
-if document.getElementById
